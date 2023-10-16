797a9a247db5a6259461dd0d050409148243926d
Alex God <amborkhodoev@edu.hse.ru>
2023-10-16 20:02:31 +0300

    added beerDTO

--- ios_practical/ViewController.swift
+++ ios_practical/ViewController.swift
@@ -14,29 +14,24 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
         
-        let url : URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
         URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
             guard 
                 let data,
                 error == nil
             else { return }
-//            let decoder = JSONDecoder()
-            let str = String(data: data, encoding: .utf8)
-            print(str ?? "")
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let model = try! decoder.decode([BeerDTO].self, from: data)
+            print(model)
         }).resume()
         
     }
-
 }
 
-//class Networking {
-//    
-//    let session: URLSession = .shared
-//    
-//    func performRequest(url: URL) {
-//        session.dataTask(with: url) { data, response, error in
-//
-//        }
-//    }
-//    
-//}
+struct BeerDTO: Decodable {
+    let id: Int
+    let name: String
+    let tagline: String
+    let imageUrl: URL
+}
