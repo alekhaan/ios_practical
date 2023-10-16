e975c84508c1d42145c90449daa2c4e0ea2b9579
Alex God <amborkhodoev@edu.hse.ru>
2023-10-16 19:42:31 +0300

    added url session

--- ios_practical/ViewController.swift
+++ ios_practical/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 final class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         view.backgroundColor = .systemMint
+        
+        let url : URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard 
+                let data,
+                error == nil
+            else { return }
+//            let decoder = JSONDecoder()
+            let str = String(data: data, encoding: .utf8)
+            print(str ?? "")
+        }).resume()
+        
     }
 
 }
 
+//class Networking {
+//    
+//    let session: URLSession = .shared
+//    
+//    func performRequest(url: URL) {
+//        session.dataTask(with: url) { data, response, error in
+//
+//        }
+//    }
+//    
+//}
