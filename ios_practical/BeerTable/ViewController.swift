7b0dde7dd69f3e74d0a3bed98096efa51a4984b2
Alex God <amborkhodoev@edu.hse.ru>
2023-10-30 19:44:39 +0300

    added BeerTableView

--- ios_practical/BeerTable/ViewController.swift
+++ ios_practical/BeerTable/ViewController.swift
@@ -8,26 +8,9 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.backgroundColor = .systemPink
-        return tableView
-    }()
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
-        
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-        
     }
     
 }
